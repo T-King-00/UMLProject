10850e7da8d0e9655ea42a76654bc1f0192aba75
Tony Riad <tonyshereen@outlook.com>
2023-03-07 15:48:59 +0000

    changes

---
+++ helperFunctions.py
@@ -0,0 +1,28 @@
+from spacy.language import Language
+import spacy
+
+@Language.component("custom_sentencizer")
+def custom_sentencizer(doc):
+    for i, token in enumerate(doc[:-1]):
+    
+        if token.text == "."  :
+          doc[i + 1].is_sent_start = True
+        elif  token.text == "As":
+          doc[i].is_sent_start = True
+        else:
+            # Explicitly set sentence start to False otherwise, to tell
+            # the parser to leave those tokens alone
+            doc[i + 1].is_sent_start = False
+    return doc
+
+nlp = spacy.load("en_core_web_lg")
+nlp.add_pipe("custom_sentencizer", before="parser")  # Insert before the parser
+
+def getSentencesFromFile(fileName):
+    doc = nlp(file)
+    sentences=[]
+    for sent in doc.sents:
+        sentences.append(sent.text)
+    return sentences
+
+
